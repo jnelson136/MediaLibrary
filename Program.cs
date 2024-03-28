44f53f5aa57c1db36aedc59da580ece1c3560735
Joe Nelson <jnelson136@my.wctc.edu>
2024-03-28 15:40:08 -0500

    Added Run Time Display

--- Program.cs
+++ Program.cs
@@ -11,4 +11,80 @@ string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
 
+string choice = "";
+do
+{
+    // display choices to user
+    Console.WriteLine("1) Add Movie");
+    Console.WriteLine("2) Display All Movies");
+    Console.WriteLine("Enter to quit");
+    // input selection
+    choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
+
+    if (choice == "1")
+    {
+        // Add movie
+        Movie movie = new Movie();
+        // ask user to input movie title
+        Console.WriteLine("Enter movie title");
+        // input title
+        movie.title = Console.ReadLine();
+        // verify title is unique
+        if (movieFile.isUniqueTitle(movie.title))
+        {
+            Console.WriteLine("Who is the director?");
+            movie.director = Console.ReadLine();
+
+            Console.WriteLine("What Is The Movie Runtime? h:m:s");
+            string runtimeInput = Console.ReadLine();
+
+            string[] hms = runtimeInput.Split(':');
+
+            int hours = int.Parse(hms[0]);
+            int minutes = int.Parse(hms[1]);
+            int seconds = int.Parse(hms[2]);
+
+            TimeSpan runtime = new TimeSpan(hours, minutes, seconds);
+
+            movie.runningTime = runtime;
+
+            // input genres
+            string input;
+            do
+            {
+                // ask user to enter genre
+                Console.WriteLine("Enter genre (or done to quit)");
+                // input genre
+                input = Console.ReadLine();
+                // if user enters "done"
+                // or does not enter a genre do not add it to list
+                if (input != "done" && input.Length > 0)
+                {
+                    movie.genres.Add(input);
+                }
+
+            } while (input != "done");
+            // specify if no genres are entered
+            if (movie.genres.Count == 0)
+            {
+                movie.genres.Add("(no genres listed)");
+            }
+            // add movie
+            movieFile.AddMovie(movie);
+        }
+
+    }
+    else if (choice == "2")
+    {
+        // Display All Movies
+        foreach (Movie m in movieFile.Movies)
+        {
+            Console.WriteLine(m.Display());
+        }
+    }
+
+} while (choice == "1" || choice == "2");
+
+
 logger.Info("Program ended");
