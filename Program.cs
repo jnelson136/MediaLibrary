bc35657d40346d00700028eacf0908d7c6f37a48
Joe Nelson <jnelson136@my.wctc.edu>
2024-03-28 13:27:25 -0500

    NLog

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Test");
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+Console.WriteLine("Hello World!");
+
+logger.Info("Program ended");
